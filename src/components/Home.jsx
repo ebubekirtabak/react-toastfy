b03b9f57caee82f5f155f247fee2e27999106ac0
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-02-03 17:05:11 +0300

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const formatName = user => `${user.firstName} ${user.lastName}`;
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe'
+};
+
+const Home = () => (
+  <div>
+    <h1>React Toastfy! </h1>
+
+  </div>
+)
+
+
+export default Home;
---
+++ src/components/toast-creator/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="toast-creator">
+    <h3> Create New Toast </h3>
+    <div className="toast-creator__content">
+    
+    </div>
+  </div>
+)
+
+
+export default Home;
