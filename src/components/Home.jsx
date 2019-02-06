386d30a8892ed00a87b434d04b14995458acbb83
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-02-06 22:01:15 +0300

    Added form in toastcreator

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import ToastCreator from './ToastCreator';
 
-const formatName = user => `${user.firstName} ${user.lastName}`;
-
-const user = {
-  firstName: 'John',
-  lastName: 'Doe'
-};
-
 const Home = () => (
   <div>
     <h1>React Toastfy! </h1>
--- src/components/ToastCreator.jsx
+++ src/components/ToastCreator.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-
-const ToastCreator = () => (
-  <div className="toast-creator">
-    <h3> Create New Toast </h3>
-    <div className="toast-creator__content">
-
-    </div>
-  </div>
-)
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
 
-export default ToastCreator;
+export default class ToastCreator extends Component {
+
+  render () {
+   return (
+     <div className="toast-creator">
+       <h3> Create New Toast </h3>
+       <div className="toast-creator__content">
+         <form>
+           <label>
+             Name:
+           </label>
+           <input type="text" name="name" />
+
+           <input type="submit" value="Submit" />
+         </form>
+       </div>
+     </div>
+   );
+ }
+}
