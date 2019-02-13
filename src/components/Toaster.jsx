d8c019f82c6860da268c74be0f4ec2efd21c2dc2
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-02-13 22:16:52 +0300

    Added simple toast list.

--- src/components/Toaster.jsx
+++ src/components/Toaster.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import SimpleToast from './toasts/SimpleToast';
 
 
 export default class Toaster extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { numbers: [1, 2, 3, 4, 5] };
+  }
+
   render () {
     return (
       <div className="toast-container tr">
-
+        <div className="toast-container__content">
+          {this.state.numbers.map((number) =>
+            <SimpleToast key={number} />
+          )}
+        </div>
       </div>
     );
   }
 
   componentDidMount () {
     console.log('onload');
+  }
+
+  appendToast() {
+    this.setState(state => ({
+      numbers: [...state.numbers, 12]
+    }));
   }
 }
--- src/components/toasts/SimpleToast.jsx
+++ src/components/toasts/SimpleToast.jsx
@@ -6,10 +6,12 @@ export default class SimpleToast extends Component {
 
   render () {
     return (
-      <div className="simple-toast">
+      <div className="simple-toast rounded">
         <div className="simple-toast__title">
+          Simple Toast Title
         </div>
         <div className="simple-toast__message">
+          Lorem ipsum message text.
         </div>
         <div className="simple-toast__icon">
         </div>
