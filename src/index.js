d14cf38cfb779d33df361d966d6387761dd9651e
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-02-20 23:15:32 +0300

    Eslint fix.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import App from './App';
 const store = createStore(toastApp);
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+	<Provider store={store}>
+		<App />
+	</Provider>,
   document.getElementById('root')
 )
