b895e6d517348eda8e0fd4ce4703e93e42d57f1b
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-02-25 23:42:26 +0300

    ToastCreattor connect to addSimpleToast.

--- src/components/ToastCreator.jsx
+++ src/components/ToastCreator.jsx
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
+import { addSimpleToast } from '../store/actions';
+import { connect } from 'react-redux';
 
-export default class ToastCreator extends Component {
+class ToastCreator extends Component {
 
-render () {
-  return (
-      <div className="toast-creator">
-          <h3> Create New Toast </h3>
-          <div className="toast-creator__content">
-              <form>
-                  <div className="input-content">
-                      <label>
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '' ,
+            message: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render () {
+        return (
+            <div className="toast-creator">
+                <h3> Create New Toast </h3>
+                <div className="toast-creator__content">
+                    <form onSubmit={ this.handleSubmit }>
+                        <div className="input-content">
+                            <label>
                         Title:
-                      </label>
-                      <input type="text" name="Title" />
-                  </div>
-                  <div className="input-content">
-                      <label>
+                            </label>
+                            <input type="text" name="title" value={ this.state.title } onChange={ this.handleChange }  />
+                        </div>
+                        <div className="input-content">
+                            <label>
                         Message:
-                      </label>
-                      <input type="text" name="Message" />
-                  </div>
-                  <input type="submit" value="Submit" />
-              </form>
-          </div>
-      </div>
-    );
-  }
+                            </label>
+                            <input type="text" name="message" value={ this.state.message } onChange={ this.handleChange } />
+                        </div>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+    handleChange (evt) {
+        this.setState({ [ evt.target.name ]: evt.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const id = Math.floor(Math.random() * 24234234);
+        this.props.addSimpleToast(id, 'Test => ' + id );
+    }
 }
+
+const mapDispatchToProps = {
+    addSimpleToast
+};
+
+export default connect( null, mapDispatchToProps )( ToastCreator );
