cf3e7c47fd3c579076e2676b978f94b630254906
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-02-26 20:12:03 +0300

    ToastOnDestroy added.

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,11 +2,16 @@ import {
 	ADD_SIMPLE_TOAST,
 	CLEAR_SIMPLE_TOAST,
 	TOAST_OKAY_ACTION,
-	TOAST_DENY_ACTION
+	TOAST_DENY_ACTION,
+	TOAST_ON_DESTROY
 } from './actionTypes';
 
 export function addSimpleToast(toast) {
 	return { type: ADD_SIMPLE_TOAST, toast };
+}
+
+export toastOnDestrOy(toast) {
+	return { type: TOAST_ON_DESTROY, toast}
 }
 
 export function clearSimpleToast(id) {
